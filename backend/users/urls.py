1fa244f9d99283c68bbecb5db0279eea02e0e57a
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-11-22 09:46:14 +0530

    updated backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+from users.views import RegisterView
 
-]
+router = DefaultRouter()
+router.register('register',RegisterView,basename='register')
+urlpatterns = router.urls
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class RegisterView(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Create your views here.
